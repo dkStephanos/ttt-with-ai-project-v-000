71fb366d6abeb48c2eccbac048dc5baff3ba2417
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-06 15:36:54 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,3 +1,22 @@
 class Human < Player
 
+  def move(board)
+    puts "Player, Please enter 1-9:"
+    input = gets.strip
+    index = board.input_to_index(input)
+
+    is_valid = false
+    until is_valid
+      if board.valid?(index)
+        board.move(index, player)
+        board.display
+        is_valid = true
+      else
+        puts "Invalid Selection! Please enter 1-9:"
+        input = gets.strip
+        index = board.input_to_index(input)
+      end
+    end
+  end
+
 end
