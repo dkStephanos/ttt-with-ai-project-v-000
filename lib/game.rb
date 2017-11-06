272978bcf3d4cdf70f78bcc0d0f9285102eb3f09
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-06 16:30:50 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -125,6 +125,7 @@ class Game
         @board.display
         is_valid = true
       else
+        binding.pry
         puts "Invalid Selection! Please enter 1-9:"
         position = gets.chomp
       end
