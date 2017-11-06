6f47923700bb54312ec8f16b8a7f465297a4c797
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-06 15:43:29 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,19 +3,7 @@ class Players::Human < Player
   def move(board)
     puts "Player, Please enter 1-9:"
     input = gets.chomp
-    index = input.to_i - 1
-
-    is_valid = false
-    until is_valid
-      if board.valid_move?(index)
-        is_valid = true
-      else
-        puts "Invalid Selection! Please enter 1-9:"
-        input = gets.chomp
-        index = input.to_i - 1
-      end
-    end
-    input
+    #index = input.to_i - 1
   end
 
 end
