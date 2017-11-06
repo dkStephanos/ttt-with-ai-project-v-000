8e93462a077642f6c850da2c062228604bb8cf58
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-06 16:24:07 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,7 @@ class Players::Computer < Player
 
   def move(board)
     current_move = 0
-    1..9.each do |cell|
+    (1..9).each do |cell|
       if board.valid_move?(cell)
         current_move = cell
       end
